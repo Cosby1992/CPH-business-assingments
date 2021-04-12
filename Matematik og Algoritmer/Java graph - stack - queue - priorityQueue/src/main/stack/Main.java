e896aad784a0b6a1b71baa2d07c0d3b6a68babad
cosby1992 <andersbirkedahl@hotmail.com>
2021-04-12 16:48:32 +0200

    Renamed parent folder

--- Matematik og Algoritmer/Java graph - stack - queue - priorityQueue/src/main/stack/Main.java
+++ Matematik og Algoritmer/Java graph - stack - queue - priorityQueue/src/main/stack/Main.java
@@ -18,6 +18,9 @@ public class Main {
         stack.push(6); stack.push(6);
         stack.push(5); stack.push(5);
         stack.push(4); stack.push(4);
+        stack.push(3); stack.push(3);
+        stack.push(2); stack.push(2);
+        stack.push(1); stack.push(1);
         
         while(!stack.isEmpty()) {
             System.out.println(stack.pop());
