e25314be7c726bc7c266da5ec0342c93fd50fab5
cosby1992 <andersbirkedahl@hotmail.com>
2021-04-12 16:44:17 +0200

    comments added to interface

--- Matematik og Algoritmer/Java Graph/src/main/graph/Graph.java
+++ Matematik og Algoritmer/Java Graph/src/main/graph/Graph.java
@@ -5,9 +5,9 @@ package main.graph;
  */
 public interface Graph {
 
-    int getV();
-    int getE();
-    void addEdge(int v1, int v2);
-    Iterable<Integer> adj(int v);
+    int getV(); // Get vertices count (number of nodes in graph)
+    int getE(); // Get the number of edges in the graph (edges between nodes)
+    void addEdge(int v1, int v2); // add an edge from vertice v1 to vertice v2
+    Iterable<Integer> adj(int v); // Get connections (edges) from a vertice (return as Iterable containing the vertices it is connected with)
 
 }
