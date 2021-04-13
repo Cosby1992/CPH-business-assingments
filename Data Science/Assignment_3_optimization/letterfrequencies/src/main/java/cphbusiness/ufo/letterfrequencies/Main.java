564027977a8712ad96c19d82f1a4705520301858
cosby1992 <andersbirkedahl@hotmail.com>
2021-04-13 22:21:34 +0200

    fixed classpaths

--- Data Science/Assignment_3_optimization/letterfrequencies/src/main/java/cphbusiness/ufo/letterfrequencies/Main.java
+++ Data Science/Assignment_3_optimization/letterfrequencies/src/main/java/cphbusiness/ufo/letterfrequencies/Main.java
@@ -15,48 +15,30 @@ import static java.util.stream.Collectors.toMap;
  * https://en.wikipedia.org/wiki/Frequency_analysis
  *
  * @author kasper
- * 
  */
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-
-        Timer timer = new Timer();
-
-        
         String fileName = "letterfrequencies/src/main/resources/FoundationSeries.txt";
         Reader reader = new FileReader(fileName);
         Map<Integer, Long> freq = new HashMap<>();
-        timer.start();
         tallyChars(reader, freq);
-        long step = timer.step();
-        System.out.println("Tally chars execution time: " + step / 1_000_000 + "ms");
-
         print_tally(freq);
-
-        long stop = timer.stop();
-        System.out.println("Tally chars execution time: " + stop / 1_000_000 + "ms");
     }
 
     private static void tallyChars(Reader reader, Map<Integer, Long> freq) throws IOException {
         int b;
         while ((b = reader.read()) != -1) {
-            
             try {
                 freq.put(b, freq.get(b) + 1);
             } catch (NullPointerException np) {
                 freq.put(b, 1L);
             };
-            
         }
-
-        System.out.println(freq);
-
     }
 
     private static void print_tally(Map<Integer, Long> freq) {
         int dist = 'a' - 'A';
-        System.out.println(dist);
         Map<Character, Long> upperAndlower = new LinkedHashMap();
         for (Character c = 'A'; c <= 'Z'; c++) {
             upperAndlower.put(c, freq.getOrDefault(c, 0L) + freq.getOrDefault(c + dist, 0L));
--- Data Science/Assignment_3_optimization/letterfrequencies/src/main/java/cphbusiness/ufo/letterfrequencies/MainOptimized.java
+++ Data Science/Assignment_3_optimization/letterfrequencies/src/main/java/cphbusiness/ufo/letterfrequencies/MainOptimized.java
@@ -1,4 +1,4 @@
-package cphbusiness.ufo.letterfrequencies;
+package src.main;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
