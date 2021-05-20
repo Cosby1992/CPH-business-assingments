1376b00282bf7a1462984784db519526063d6246
stabz <dmitrokaraush@gmail.com>
2021-05-20 17:59:18 +0200

    post add

--- Databases/twitter-clone/src/main/java/dk/cphbusiness/mrv/twitterclone/impl/PostManagementImpl.java
+++ Databases/twitter-clone/src/main/java/dk/cphbusiness/mrv/twitterclone/impl/PostManagementImpl.java
@@ -22,16 +22,51 @@ public class PostManagementImpl implements PostManagement {
 
     @Override
     public boolean createPost(String username, String message) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        boolean exists = jedis.exists("user:"+username );
+        
+        if(exists){
+            jedis.set("post:"+username+":"+time, message);
+            //jedis.rpush("post:"+username+":posttime",""+time);
+
+            jedis.zadd("post:"+username+":posttime",0,String.valueOf(time));
+            exists = true;
+        }
+
+        return exists;
     }
 
     @Override
     public List<Post> getPosts(String username) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        boolean exists = jedis.exists("user:" + username);
+        if(!exists) return null;
+        
+        Set<String> timestamps = jedis.zrange("post:"+username+":posttime", 0, -1);
+        List<Post> postList = new ArrayList<Post>();
+
+        for (String ts : timestamps) {
+
+            postList.add(new Post(Long.parseLong(ts), jedis.get("post:"+username+":"+ts)));
+        }
+
+        return postList;
     }
 
     @Override
     public List<Post> getPostsBetween(String username, long timeFrom, long timeTo) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        boolean exists = jedis.exists("user:" + username);
+        if(!exists) return null;
+
+        long start = 0;
+                
+
+        Set<String> timestamps = jedis.zrangeByLex("post:"+username+":posttime", String.valueOf(timeFrom-1), String.valueOf(timeTo+1));
+        List<Post> postList = new ArrayList<Post>();
+
+        for (String ts : timestamps) {
+
+            postList.add(new Post(Long.parseLong(ts), jedis.get("post:"+username+":"+ts)));
+        }
+
+        return postList;
     }
 }
